06dd2423924e10f1b0e72eb1d40d26c889dad4d6
Piyush Bachani <Piyush>
2026-02-05 16:07:51 +0530

    Connected Postgres Database.

--- server/app/backend/api/hello/route.ts
+++ server/app/backend/api/hello/route.ts
@@ -1,5 +1,19 @@
 import { NextResponse } from "next/server";
+import pool from "../../lib/db";
 
-export function GET() {
-  return NextResponse.json({ message: "Hello World!" });
+export async function GET() {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    
+    return NextResponse.json({
+      message: "hello World",
+      dbstatus: "Connected",
+      time: result.rows[0].now,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Database connection failed" },
+      { status: 500 }
+    );
+  }
 }
